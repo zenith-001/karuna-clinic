6972e7921320e3eef33fa5869199d4c6f1bdb72c
Zenith Kandel <kandelze123@gmail.com>
2024-11-08 15:42:01 +0545

    cleanup

--- JS/index.js
+++ JS/index.js
@@ -5,24 +5,3 @@ if (window.pageYOffset <=10) {
 else{
     document.querySelector("#go-up").style.display="block";
 }}
-const themeToggleBtn = document.getElementById("theme-toggle");
-const htmlElement = document.documentElement;
-
-themeToggleBtn.addEventListener("click", () => {
-  // Toggle the "dark" class on the <html> element
-  htmlElement.classList.toggle("dark");
-  
-  // Optionally, save the theme choice to localStorage
-  if (htmlElement.classList.contains("dark")) {
-    localStorage.setItem("theme", "dark");
-  } else {
-    localStorage.setItem("theme", "light");
-  }
-});
-
-// Load theme from localStorage on page load
-if (localStorage.getItem("theme") === "dark") {
-  htmlElement.classList.add("dark");
-} else {
-  htmlElement.classList.remove("dark");
-}
